7be83a52f89b53e7bf1182b48fc8e2f430bc8cf1
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-27 23:37:12 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,10 +1,33 @@
-class Author 
- attr_accessor:name,:posts
- 
- def initialize(name)
-   @name = name
-   @posts = []
- end 
- 
- 
-end 
+class Artist
+
+  attr_accessor :name, :posts
+
+  @@post_count = 0
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def posts
+    Post.all.select {|post| post.artist == self}
+  end
+  
+  def add_post(post)
+    @posts << post
+    post.artist = self
+    @@posts += 1
+  end
+
+  def add_post_by_title(title)
+    @@posts += 1
+    p = Post.new(title)
+    self.add_post(p)
+    p
+  end
+  
+  def self.song_count
+    @@song_count 
+  end 
+end
+   

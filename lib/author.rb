a001c2a4cdf4ed2b4ed44d21812929b73151ffbc
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-27 23:38:04 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -12,22 +12,4 @@ class Artist
   def posts
     Post.all.select {|post| post.artist == self}
   end
-  
-  def add_post(post)
-    @posts << post
-    post.artist = self
-    @@posts += 1
-  end
-
-  def add_post_by_title(title)
-    @@posts += 1
-    p = Post.new(title)
-    self.add_post(p)
-    p
-  end
-  
-  def self.post_count
-    @@post_count 
-  end 
-end
-   
+end 

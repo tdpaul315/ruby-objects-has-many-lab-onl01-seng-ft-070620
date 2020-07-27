2ca9050205db7e1f58f5c9e6c73162f29953dea9
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-27 23:23:12 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,12 +20,12 @@ class Artist
   end
 
   def add_song_by_name(name)
-    song = Song.new(name)
-    @songs << song
-    song.artist = self
     @@song_count += 1
+    s = Song.new(name)
+    self.add_song(s)
+    s
   end
-
+  
   def self.song_count
     @@song_count 
   end 
